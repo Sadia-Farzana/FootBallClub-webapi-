9234fe4c75c3e0f06ae7c96ef722a63b1102933a
17-33890-1 <17-33890-1@student.aiub.edu>
2020-09-25 03:23:56 +0600

    crud

---
+++ FootBallClub/FootBallClub/Models/FinalDataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace FootBallClub.Models
+{
+    public class FinalDataContext : DbContext 
+    {
+        public FinalDataContext()
+        {
+            Database.SetInitializer<FinalDataContext>(new DropCreateDatabaseAlways<FinalDataContext>());
+        }
+
+        virtual public DbSet<Player> Players { get; set; }
+    }
+}
